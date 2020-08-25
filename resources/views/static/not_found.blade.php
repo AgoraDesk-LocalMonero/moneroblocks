9fb5a3198f18f01f9c5843fd7c3203fb65fa18e2
root <root@lm-block-explorer.c.localmonero.internal>
2020-08-25 21:40:10 +0000

    corrected blade ternary operator deprecated syntax

--- resources/views/static/not_found.blade.php
+++ resources/views/static/not_found.blade.php
@@ -15,7 +15,7 @@
   <div class="col-lg-12">
     <p>
       {!! isset($query) ? "Shame on you!<br>There is no such thing as $query in Monero's blockchain!" :
-        isset($message) ? $message : "Nothing found." !!}
+        (isset($message) ? $message : "Nothing found.") !!}
     </p>
   </div> <!-- /.col-lg-12 -->
 </div> <!-- ./row -->
